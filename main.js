0042522d902ebf649a3ad4cd6e7e0351dbebfe09
blibotics <blibotics@gmail.com>
2014-01-11 01:55:47 -0500

    Created some functions to return info about the classes

--- main.js
+++ main.js
@@ -1,6 +1,29 @@
 function School(){
-  this.classes = {
-    total: 0,
-    students: 0
+  this.classes = [];
+  var classInstance = {};
+  this.makeClass =  function(courseName, students, cost, prof, salary, ta, wage, hours){
+    var totalPaid = students * cost;
+    var totalExpenses = (prof * salary) + (ta * wage * hours);
+    var totalRevenue = totalPaid - totalExpenses;
+    classInstance.name = courseName;
+    classInstance.total = totalRevenue;
+    classInstance.students = students;
+    classInstance.cost = cost;
+    classInstance.prof = prof;
+    classInstance.salary = salary;
+    classInstance.ta = ta;
+    classInstance.wage = wage;
+    classInstance.hours = hours; 
+    this.classes.push(classInstance);
+    classInstance = {};
+
+    var returnStatement = "The course name is " + courseName + " with " + students + " students."
+    return returnStatement;
+  };
+  this.classInfo = function(obj){
+      return obj;
+  };
+  this.numClasses = function(){
+    return this.classes.length;
   };
 }
